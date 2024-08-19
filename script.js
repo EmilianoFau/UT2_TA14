bed215eafeb9c021011d613dd5c34b322bfc4147
EmilianoFau <emiliano.fau@correo.ucu.edu.uy>
2024-08-19 10:00:01 -0300

    a

--- script.js
+++ script.js
@@ -3,13 +3,11 @@ function anadirElemento() {
     const input = document.getElementById("input");
     const lista = document.getElementById("list");
 
-    boton.addEventListener("click", function(){
-        let valor = input.value;
-        if (valor) {
-            let nuevoElemento = document.createElement("li");
-            nuevoElemento.textContent = valor;
-            lista.appendChild(nuevoElemento);
-            input.value = "";
-        }
-    });
+    let valor = input.value;
+    if (valor) {
+        let nuevoElemento = document.createElement("li");
+        nuevoElemento.textContent = valor;
+        lista.appendChild(nuevoElemento);
+        input.value = "";
+    }
 }
